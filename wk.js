ab7bd2c0cf8c57ce86ff3f974c90c074ef99c1e9
byxiaoxiao520 <70189157+byxiaoxiao520@users.noreply.github.com>
2022-04-25 15:44:08 +0800

    Create wk.js

---
+++ wk.js
@@ -0,0 +1,14 @@
+{
+  "error": 0,
+  "data": {
+    "error_msg": "",
+    "is_success": true,
+    "user_info": {
+      "is_vip": "9A0684792021D73BE42B71491469ADFA",
+      "expired_time": 0,
+      "member_state": 2
+    },
+    "error_code": "",
+    "data_info": []
+  }
+}
